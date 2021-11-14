f4a439fa2dddc62d9048d4b7f9cfeec6c4b1fb14
Kyle Schwartz <kyleschwartz2000@gmail.com>
2021-11-14 13:56:38 -0500

    Update generate_winrm.sh

--- scripts/generate_winrm.sh
+++ scripts/generate_winrm.sh
@@ -10,5 +10,5 @@ subjectAltName = otherName:1.3.6.1.4.1.311.20.2.3;UTF8:$USERNAME@localhost
 EOL
 
 export OPENSSL_CONF=openssl.conf
-openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -out ~/.ssh/cert.pem -outform PEM -keyout ~/.ssh/cert_key.pem -subj "/CN=$USERNAME" -extensions v3_req_client
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -out ~/.ssh/winrm.pem -outform PEM -keyout ~/.ssh/winrm_key.pem -subj "/CN=$USERNAME" -extensions v3_req_client
 rm openssl.conf
